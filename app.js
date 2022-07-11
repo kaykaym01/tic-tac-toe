ebe9d6041df026afc35dfde9e45997e218fd9798
kaykaym01 <kaymoore01@gmail.com>
2022-07-11 14:18:54 -0400

    Add displayController and displayBoard()

--- app.js
+++ app.js
@@ -16,14 +16,25 @@ let gameboard = (function () {
 
     /**
      * Adds a marker on the board at position X, Y
-     * @param {*} x 
-     * @param {*} y 
-     * @param {*} marker 
+     * @param {number} x Row position 
+     * @param {number} y Column position
+     * @param {string} marker String value for marker
      */
     function addMarker(x, y, marker) {
         board[x][y] = marker;
     }
     return { board, addMarker };
+})();
+
+let displayController = (function() {
+    /**
+     * Can display the gameboard
+     * @param {*} gameboard 
+     */
+    function displayBoard(board){
+        console.log(board);
+    }
+    return {displayBoard};
 })();
 
 game.start();

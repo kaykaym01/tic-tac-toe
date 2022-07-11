1d4f6459921726d192bfcd2a47ceeb22da4f88e5
kaykaym01 <kaymoore01@gmail.com>
2022-07-11 16:04:29 -0400

    Add _checkForWinner and update displayController

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let game = (function () {
      */
     function start() {
         console.log("Game Started");
+        displayController.displayBoard(gameboard.board);
     }
 
     return { start };
@@ -22,17 +23,93 @@ let gameboard = (function () {
      */
     function addMarker(x, y, marker) {
         board[x][y] = marker;
+        displayController.displayBoard(gameboard.board);
+        if (_checkForWinner(x, y, marker)){
+            console.log("Winner is: " + marker);
+        }
+    }
+
+    /**
+     * 
+     * @param {*} lastX 
+     * @param {*} lastY 
+     * @param {*} marker 
+     * @returns 
+     */
+    function _checkForWinner(lastX, lastY, marker){
+        let n = board.length;
+        
+        // check for row winner
+        for (let i = 0; i < n; i++){
+            if (board[lastX][i] != marker){
+                break;
+            }
+            if (i == n-1){
+                return true;
+            }
+        }
+
+        // check for column winner
+        for (let i = 0; i < n; i++){
+            if (board[i][lastY] != marker){
+                break;
+            }
+            if (i == n-1){
+                return true;
+            }
+        }
+
+        //if on positive diagonal, check for diagonal winner
+        if (lastX == lastY){
+            for (let i = 0; i < n; i++){
+                if (board[i][i] != marker){
+                    break;
+                }
+                if (i == n-1){
+                    return true;
+                }
+            }
+        }
+
+        // if on anti-diagonal, check for diagonal winner
+        if (lastX+lastY==n-1){
+            for (let i = 0; i < n; i++){
+                if (board[i][n-1-i] != marker){
+                    break;
+                }
+                if (i == n-1){
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
     return { board, addMarker };
 })();
 
 let displayController = (function() {
     /**
-     * Can display the gameboard
+     * Can display the gameboard in the console
      * @param {*} gameboard 
      */
     function displayBoard(board){
-        console.log(board);
+        let n = board.length;
+        let display = "";
+        for (let i = 0; i < n; i++){
+            let row="";
+            for (let j = 0; j < n; j++){
+                row += (board[i][j]) ? board[i][j] : "_";
+                if (j < n-1){
+                    row += "  ";
+                }
+            }
+            display += row;
+            if (i < n-1){
+                display += "\n";
+            }
+        }
+        console.log(display)
     }
     return {displayBoard};
 })();

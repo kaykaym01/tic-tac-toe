2c5764e76cc26c1bcdafb4398aed632d48bc3858
kaykaym01 <kaymoore01@gmail.com>
2022-07-14 22:42:25 -0400

    Add game.end() to end game

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const playerFactory = (name, marker) => {
      * Player takes their turn and adds a marker to the gameboard
      * @param {*} gameboard 
      */
-    function takeTurn(gameboard){
+    function takeTurn(gameboard) {
         let posX = prompt("Select the row position of your marker");
         let posY = prompt("Select the column position of your marker");
         console.log(name + " Turn");
-        gameboard.addMarker(parseInt(posX), parseInt(posY), marker); 
+        gameboard.addMarker(parseInt(posX), parseInt(posY), marker);
     }
 
     /**
      * Player changes their name from name to newName
      * @param {*} newName 
      */
-    function setName(newName){
+    function setName(newName) {
         name = newName;
     }
 
-    return {name, marker, takeTurn, setName};
+    return { name, marker, takeTurn, setName };
 }
 
 let game = (function () {
@@ -34,12 +34,20 @@ let game = (function () {
     /**
      * Restarts the game
      */
-    function restart(){
+    function restart() {
         console.log("Restarting game");
         start();
     }
 
-    return { start, restart};
+    /**
+     * Ends the game
+     */
+    function end() {
+        console.log("Ending game");
+        gameboard.endGame();
+    }
+
+    return { start, restart };
 })();
 
 let gameboard = (function () {
@@ -57,21 +65,25 @@ let gameboard = (function () {
      * @param {string} marker String value for marker
      */
     function addMarker(x, y, marker) {
-        board[x][y] = marker;
-        _movesTaken++;
-        displayController.displayBoard(gameboard.board);
-        if (_checkForWinner(x, y, marker)){
-            console.log("Winner is: " + marker);
-            _gameOver = true;
-        } else if (_checkForTiedGame()){
-            console.log("Draw");
-            _gameOver = true;
+        if (_gameOver) {
+            console.log("Game is over. Start new game to continue playing.")
+        } else {
+            board[x][y] = marker;
+            _movesTaken++;
+            displayController.displayBoard(gameboard.board);
+            if (_checkForWinner(x, y, marker)) {
+                console.log("Winner is: " + marker);
+                _gameOver = true;
+            } else if (_checkForTiedGame()) {
+                console.log("Draw");
+                _gameOver = true;
+            }
         }
     }
 
     /**
-     * 
-     * @returns true if the game has ended in a tie
+     * Checks if the game is tied 
+     * @returns true if the number of max moves has been made (tied game)
      */
     function _checkForTiedGame() {
         return _maxMoves == _movesTaken;
@@ -84,48 +96,48 @@ let gameboard = (function () {
      * @param {*} marker 
      * @returns 
      */
-    function _checkForWinner(lastX, lastY, marker){
+    function _checkForWinner(lastX, lastY, marker) {
         let n = board.length;
-        
+
         // check for row winner
-        for (let i = 0; i < n; i++){
-            if (board[lastX][i] != marker){
+        for (let i = 0; i < n; i++) {
+            if (board[lastX][i] != marker) {
                 break;
             }
-            if (i == n-1){
+            if (i == n - 1) {
                 return true;
             }
         }
 
         // check for column winner
-        for (let i = 0; i < n; i++){
-            if (board[i][lastY] != marker){
+        for (let i = 0; i < n; i++) {
+            if (board[i][lastY] != marker) {
                 break;
             }
-            if (i == n-1){
+            if (i == n - 1) {
                 return true;
             }
         }
 
         //if on positive diagonal, check for diagonal winner
-        if (lastX == lastY){
-            for (let i = 0; i < n; i++){
-                if (board[i][i] != marker){
+        if (lastX == lastY) {
+            for (let i = 0; i < n; i++) {
+                if (board[i][i] != marker) {
                     break;
                 }
-                if (i == n-1){
+                if (i == n - 1) {
                     return true;
                 }
             }
         }
 
         // if on anti-diagonal, check for diagonal winner
-        if (lastX+lastY==n-1){
-            for (let i = 0; i < n; i++){
-                if (board[i][n-1-i] != marker){
+        if (lastX + lastY == n - 1) {
+            for (let i = 0; i < n; i++) {
+                if (board[i][n - 1 - i] != marker) {
                     break;
                 }
-                if (i == n-1){
+                if (i == n - 1) {
                     return true;
                 }
             }
@@ -137,44 +149,52 @@ let gameboard = (function () {
     /**
      * Resets the gameboard to be a 3x3 empty array
      */
-    function resetBoard(){
+    function resetBoard() {
+        _gameOver = false;
         board.splice(0, board.length);
         let n = 3;
-        for (let i = 0; i < n; i++){
+        for (let i = 0; i < n; i++) {
             let row = [];
-            for (let j = 0; j < n; j++){
+            for (let j = 0; j < n; j++) {
                 row.push("");
             }
             board.push(row);
         }
     }
-    return { board, addMarker, resetBoard};
+
+    /**
+     * Ends the game
+     */
+    function endGame() {
+        _gameOver = true;
+    }
+    return { board, addMarker, resetBoard, endGame };
 })();
 
-let displayController = (function() {
+let displayController = (function () {
     /**
      * Displays the gameboard in the console
      * @param {*} gameboard 
      */
-    function displayBoard(board){
+    function displayBoard(board) {
         let n = board.length;
         let display = "";
-        for (let i = 0; i < n; i++){
-            let row="";
-            for (let j = 0; j < n; j++){
+        for (let i = 0; i < n; i++) {
+            let row = "";
+            for (let j = 0; j < n; j++) {
                 row += (board[i][j]) ? board[i][j] : "_";
-                if (j < n-1){
+                if (j < n - 1) {
                     row += "  ";
                 }
             }
             display += row;
-            if (i < n-1){
+            if (i < n - 1) {
                 display += "\n";
             }
         }
         console.log(display)
     }
-    return {displayBoard};
+    return { displayBoard };
 })();
 
 game.start();
